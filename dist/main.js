27234cb76d70ec17584542123b4534e377e6673a
Miguelangel Dominguez <mdominguez@queplan.cl>
2024-01-09 20:21:24 -0400

    deploy

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,7 @@
 "use strict";
-require('events').EventEmitter.defaultMaxListeners = 25;
+require('events').EventEmitter.defaultMaxListeners = 100;
+require('events').EventEmitter.prototype._maxListeners = 100;
+
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
