1c1812cef9cc92fe4e0c3d41f7c1eb6f677a8891
Miguelangel Dominguez <mdominguez@queplan.cl>
2024-01-11 22:16:29 -0400

    CORS

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.enableCors();
+
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT);
 }
