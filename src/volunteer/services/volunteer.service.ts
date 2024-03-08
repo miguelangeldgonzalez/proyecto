05da2ce0dd1a154908010b84ac5cee71d304f461
Miguelangel Dominguez <mdominguez@queplan.cl>
2024-03-07 22:13:05 -0400

    Refactor createVolunteer method and add addWorkdayToExistVolunteer method

--- src/volunteer/services/volunteer.service.ts
+++ src/volunteer/services/volunteer.service.ts
@@ -41,10 +41,8 @@ export class VolunteerService {
      */
     async createVolunteer(data: CreateVolunteerDTO): Promise<Volunteer> {
         const borough = await this.zoneService.findBoroughById(data.boroughId);
-        delete data.boroughId;
 
         const workday = await this.workdayService.getWorkdayById(data.workdayId);
-        delete data.workdayId;
 
         const volunteer = this.volunteerRepository.create({
             borough,
@@ -57,11 +55,49 @@ export class VolunteerService {
         } catch (error) {
             if (error instanceof QueryFailedError) {
                 if ((error.driverError.detail as string).includes('identification')) {
-                    throw new BadRequestException('Ya existe un voluntario con esta identificación');
+                    return await this.addWorkdayToExistVolunteer(data);
                 }
             }
 
             throw new InternalServerErrorException('Error al crear el voluntario');
+        }
+    }
+
+    /**
+     * 
+     * @param data 
+     * @returns 
+     */
+    private async addWorkdayToExistVolunteer(data: CreateVolunteerDTO): Promise<Volunteer> {
+        const volunteer = await this.volunteerRepository.findOne({
+            where: {
+                identification: data.identification
+            },
+            relations: ['workdays', 'workdays.workdayLocation', 'borough']
+        });
+
+        if (!volunteer) {
+            throw new NotFoundException('No se encontró el voluntario con esta identificación');
+        }
+
+        const workday = await this.workdayService.getWorkdayById(data.workdayId);
+
+        // Search if exist the workday in the volunteer
+        if (!volunteer.workdays.some(w => w.id === workday.id)) {
+            volunteer.workdays.push(workday);
+        }
+
+        if (volunteer.borough.id !== data.boroughId) {
+            volunteer.borough = await this.zoneService.findBoroughById(data.boroughId);
+        }
+
+        try {
+            return await this.volunteerRepository.save({
+                ...volunteer,
+                ...data
+            });
+        } catch (error) {
+            throw new InternalServerErrorException('Error al agregar el día de trabajo al voluntario');
         }
     }
 }
